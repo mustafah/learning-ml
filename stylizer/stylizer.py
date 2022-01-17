e928e3ceaa7466629dc551a455c60f85d2483437
Mustafah <me@mustafah.me>
2022-01-17 12:54:12 +0300

    stylizer

---
+++ stylizer/stylizer.py
@@ -0,0 +1,7 @@
+import os
+import tensorflow as tf
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+print(os)
+
+content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
+style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
